e9589611539a33c45386d964a8fc0da79315e437
chzesa <chzesa+git@gmail.com>
2025-05-18 17:21:15 +0300

    Add function to extract context window of x words

--- scripts/preprocessing/entity_context.py
+++ scripts/preprocessing/entity_context.py
@@ -1,6 +1,7 @@
 import polars as pl
 from lxml import etree
 import re
+import numpy as np
 
 def find_name(position, root):
     namespaces = {'tei': 'http://www.tei-c.org/ns/1.0'}
@@ -31,3 +32,55 @@ def process_name_node(position, unode, levels=0, transform_fn=to_text_content):
 def extract_hierarchical(position, text_id, texts_df, levels=0, transform_fn=to_text_content):
     unode = xml_from_text_id(text_id, texts_df)
     return process_name_node(position, unode, levels, transform_fn)
+
+def isword(n):
+    return n.tag == r'{http://www.tei-c.org/ns/1.0}w'
+
+def ispunct(n):
+    return n.tag == r'{http://www.tei-c.org/ns/1.0}pc' 
+
+# Returns subarray containing up to width first words (w)
+def limitwindow(nodel, width):
+    if width == 0:
+        return 0
+
+    if width > len(nodel):
+        return len(nodel)
+
+    cs = np.cumsum(np.array([isword(n) for n in nodel]))
+    index = np.argwhere(cs == width)
+
+    if index.shape[0] == 0:
+        return len(nodel)
+
+    return int(index[0].item()) + 1
+
+def extract_word_window(position, text_id, texts_df, width=0, transform_fn=to_text_content):
+    unode = xml_from_text_id(text_id, texts_df)
+    namenode = find_name(position, unode)
+
+    # TODO improve
+    pre = list() # list of preceding nodes
+    post = list() # list of subsequent nodes
+    li = pre
+
+    for node in unode.iter(r'{http://www.tei-c.org/ns/1.0}w', r'{http://www.tei-c.org/ns/1.0}pc'):
+        if node == namenode[0]:
+            li = post
+
+        if node.getparent() == namenode:
+            continue
+
+        li.append(node)
+
+    pre = pre[::-1]
+    pre_exl = pre[limitwindow(pre, width):]
+    post_exl = post[limitwindow(post, width):]
+
+    # Remove unwanted nodes from tree to enable arbitrary node-based transform
+    for node in pre_exl:
+        node.getparent().remove(node)
+    for node in post_exl:
+        node.getparent().remove(node)
+
+    return transform_fn(unode)
