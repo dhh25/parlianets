5765fb42b016654ed2f51d277517dadadbf067d2
lukatercon <luka.tercon@gmail.com>
2025-05-19 14:17:07 +0300

    Added script to extract word count from parquet file

---
+++ scripts/statistics/get_word_count_from_parquet.py
@@ -0,0 +1,27 @@
+from lxml import etree
+import os
+import sys
+import pandas as pd
+
+
+def process_file(input_file):
+    parquet_df = pd.read_parquet(input_file)
+
+    parquet_df["word_count"] = parquet_df["xml"].map(lambda x: str(x).count("</w>"))
+
+    return parquet_df
+
+
+# the script should result in an identical parquet file with an additional column containing the number of words (<w> elements)
+def main(args):
+    if len(args) != 2:
+        print('Usage\npython get_word_count_from_parquet.py input_file output_file')
+        return
+    [input_file, output_file] = args
+    result_df = process_file(input_file)
+
+    result_df.to_parquet(output_file)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
