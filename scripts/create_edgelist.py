26e725379a37bd748ea669a69d402bdb33f1f5b8
dataspider <corinna.coupette@gmail.com>
2025-05-20 12:36:36 +0300

    fix issues with create_edgelist

--- scripts/create_edgelist.py
+++ scripts/create_edgelist.py
@@ -18,10 +18,11 @@ if __name__ == '__main__':
     df = pl.read_csv(coco.COUNTRY_DATA_FILE, separator="\t").select([pl.col("name_short"), pl.col("ISO2")])
     mapping = {k.lower():v for k,v in zip(df.get_column("name_short"),df.get_column("ISO2"))} | {"us": "US", "usa": "US", "holland": "NL"}
     mapping["greece"] = "GR"
+    mapping["united kingdom"] = "GB"
     for f in sorted(glob("../ParlaMint_entities_filtered/**.parquet")):
         iso2_prelim = f.split("/")[-1].split("_")[0]
         iso2_cc = iso2_prelim[:2]
         pl.scan_parquet(f).with_columns(
             pl.col("entity").map_elements(lambda x: mapping.get(x,x), return_dtype=pl.String).alias("target_country"),
             pl.lit(iso2_cc, dtype=pl.String).alias("source_country"),
-        ).sink_parquet(f"{out_dir}/{iso2_prelim}_edges.parquet", compression="zstd")
+        ).select(pl.exclude("Session", "Meeting", "Sitting", "Term")).sink_parquet(f"{out_dir}/{iso2_prelim}_edges.parquet", compression="zstd")
