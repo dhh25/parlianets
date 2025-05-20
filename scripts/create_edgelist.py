1039db5fce47882f643e72bd38a28dcee5522e05
dataspider <corinna.coupette@gmail.com>
2025-05-20 10:10:10 +0300

    fix issues with create_edgelist

--- scripts/create_edgelist.py
+++ scripts/create_edgelist.py
@@ -16,10 +16,12 @@ if __name__ == '__main__':
     out_dir = '../ParlaMint_edgelists'
     os.makedirs(out_dir, exist_ok=True)
     df = pl.read_csv(coco.COUNTRY_DATA_FILE, separator="\t").select([pl.col("name_short"), pl.col("ISO2")])
-    mapping = {k.lower():v for k,v in zip(df.get_column("name_short"),df.get_column("ISO2"))} | {"usa": "us", "holland": "nl"}
+    mapping = {k.lower():v for k,v in zip(df.get_column("name_short"),df.get_column("ISO2"))} | {"us": "US", "usa": "US", "holland": "NL"}
+    mapping["greece"] = "GR"
     for f in sorted(glob("../ParlaMint_entities_filtered/**.parquet")):
-        iso2_cc = coco.convert(f.split("/")[-1].split("_")[0], to="iso2")
+        iso2_prelim = f.split("/")[-1].split("_")[0]
+        iso2_cc = iso2_prelim[:2]
         pl.scan_parquet(f).with_columns(
             pl.col("entity").map_elements(lambda x: mapping.get(x,x), return_dtype=pl.String).alias("target_country"),
             pl.lit(iso2_cc, dtype=pl.String).alias("source_country"),
-        ).sink_parquet(f"{out_dir}/{iso2_cc}_edges.parquet", compression="zstd")
+        ).sink_parquet(f"{out_dir}/{iso2_prelim}_edges.parquet", compression="zstd")
